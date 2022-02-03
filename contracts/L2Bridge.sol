adc1ed20d1e44ab62312fff5af6aedb6bfd839b9
jakegk22 <70069930+jakegk22@users.noreply.github.com>
2022-02-03 12:24:37 -0500

    create contract base via 'truffle innit'

---
+++ contracts/L2Bridge.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+//import "@openzeppelin/contracts/token/ERC721/";
+
+//import "@openzeppelin/contracts/utils/";
+
+contract L2Bridge {
+    
+
+}
