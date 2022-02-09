3f76fc36dae52cec15a731a86c7275b0a6623c2a
jakegk22 <70069930+jakegk22@users.noreply.github.com>
2022-02-09 15:52:04 -0500

    abi structs added

--- contracts/L2Bridge.sol
+++ contracts/L2Bridge.sol
@@ -7,6 +7,19 @@ pragma solidity ^0.8.0;
 //import "@openzeppelin/contracts/utils/";
 
 contract L2Bridge {
-    
 
+    // ABI struct based on 
+    //https://notes.ethereum.org/@vbuterin/cross_layer_2_bridges
+    struct TransferData{
+        address tokenAddress;
+        address destination;
+        uint256 amount;
+        uint256 fee; 
+        uint256 startTime;
+        uint256 feeRampup;
+    }
+
+    function getLPFee(TransferData calldata transferData, uint256 currentTime) public returns (uint256){
+
+    }
 }
